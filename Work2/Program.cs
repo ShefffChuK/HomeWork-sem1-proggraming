17b964abcea64320d8c604d07160de06e579aa78
unknown <sheffchuk-83@list.ru>
2023-03-09 19:02:40 +0500

    Work2 is done

--- Work2/Program.cs
+++ Work2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 22; // работает при a = 5, b = 7; а также a = 2, b = 10
+int b = 3;
+int c = 9;
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.WriteLine(max);
